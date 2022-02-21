409dd4e05e526ced1c0039d1a6d16df2843c22dd
jor22 <jorge22ac@gmail.com>
2022-02-21 00:09:35 -0300

    Landing Page Done

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from './Landing.module.css'
+import patita from '../assests/huella.png'
+
 
 export default function Landing() {
   return (
-    <div>
-      <div>
-        <h1>Dog's World</h1>
-        <h3>
+    <div  className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>PI Dog's World. <img src={patita} className={styles.patita}/></h1>
+        <h3 className={styles.text}>
+        “A dog is the only thing on earth that loves you more than he loves himself.”<br/>― Josh Billings
         </h3>
+        <Link to={"/dog"} >
+         <button className={styles.btn}>Go HOME!</button>
+        </Link>
+        <p className={styles.footer}>A project by Jorge Ariel Castillo</p>
       </div>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -8,10 +8,9 @@ import store from "./redux/store"
 
 
 ReactDOM.render(
-  
   // <Provider store={store}>
     <App />
-  // {/* </Provider> */}
+  // </Provider>
   ,
   document.getElementById('root')
 );
